91158c99559b1e0838bbff1278fe7c37493a240a
shobhaks6264 <109946085+shobhaks6264@users.noreply.github.com>
2025-04-21 00:00:32 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -244,4 +244,6 @@ app.get('/orders/:patientId', async (req, res) => {
 
 // start
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Listening on port ${port}`);
+});
